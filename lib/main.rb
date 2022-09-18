bfceedff565f27a8e9b50d46c2ab7e1e123e56fc
PBalazs0517 <peter.balazs0517@gmail.com>
2022-09-18 11:25:47 +0200

    fixed some bug

--- lib/main.rb
+++ lib/main.rb
@@ -2,13 +2,13 @@ require "json"
 
 class Game
 
-    attr_reader :amount_of_mistakes, :used_letters, :random_word, :answer
+    attr_reader :amount_of_mistakes, :used_letters, :answer
 
     def initialize()
         @amount_of_mistakes = 0
         @used_letters = []
         @random_word = ""
-        @random_word = File.readlines("google-10000-english-usa-no-swears.txt")[rand(0..987)].chomp until 4 < @random_word.length && @random_word.length < 13 
+        @random_word = File.readlines("google-10000-english-usa-no-swears.txt")[rand(0..9870)].chomp until 4 < @random_word.length && @random_word.length < 13 
         @answer = Array.new(@random_word.length, "*")
         @player_input = ""
         @round = 0
@@ -28,7 +28,7 @@ class Game
         until @player_input.length == 1 && ('a'..'z').any? { |l| l == @player_input} && !(@used_letters.any? { |l| l == @player_input}) 
             print "  Enter a new one: "
             @player_input = gets.chomp.downcase
-            if @player_input == "save"
+            if @player_input == "save" || @player_input == "load"
                 return
             end
         end
@@ -37,6 +37,9 @@ class Game
     end
 
     def check_input
+        if @player_input == "save"
+            return
+        end
         matching = false
         @random_word.split("").each_with_index do |l, i|
             if l == @player_input
@@ -55,7 +58,8 @@ class Game
             "amount_of_mistakes" => @amount_of_mistakes,
             "used_letters" => @used_letters,
             "random_word" => @random_word,
-            "answer" => @answer
+            "answer" => @answer,
+            "round" => @round
         }.to_s.gsub("=>", ": ")
     end
 
@@ -69,27 +73,27 @@ class Game
         @used_letters = JSON.parse(data)["used_letters"]
         @random_word = JSON.parse(data)["random_word"]
         @answer = JSON.parse(data)["answer"]
+        @round = JSON.parse(data)["round"]
     end
 
     def play
+        if @round == 0
+            puts ""
+            puts "  This is Hang Man"
+            puts "  Every round you should enter a letter."
+            puts "  The answer only contains letter that are found"
+            puts "  in the english alphabet."
+            puts "  If you type 'save' you can save the game."
+            puts "  If you type 'load' you can load in your last save."
+        end
         Board.new(self).board
         self.get_input
-        @round += 1
         if @player_input == "save"
             self.save_game
-            Board.new(self).board
             puts ""
             puts "  Game saved!"
             puts ""
-            return
-        end
-        if @player_input == "load"
-            self.load_game
-            Board.new(self).board
-            puts ""
-            puts "  Game loaded!"
-            puts ""
-            self.play
+            exit
         end
         self.check_input
         if @amount_of_mistakes == 7
@@ -97,18 +101,28 @@ class Game
             puts ""
             puts "  You lost!"
             puts ""
-            return
+            exit
         end
         if @answer.join == @random_word
             Board.new(self).board
             puts ""
             puts "  You won!"
             puts ""
-            return
+            exit
         end
+        if @player_input == "load"
+            self.load_game
+            Board.new(self).board
+            puts ""
+            puts "  Game loaded!"
+            puts "  _________________________________________________"
+            loaded_game = Game.new
+            loaded_game.load_game
+            loaded_game.play
+        end
+        @round += 1
         self.play
     end
-
 end
 
 class Board
@@ -120,12 +134,6 @@ class Board
     end
 
     def board
-        puts "  This is Hang Man"
-        puts "  Every round you should enter a letter."
-        puts "  The answer only contains letter that are found"
-        puts "  in the english alphabet."
-        puts "  If you type 'save' you can save the game."
-        puts "  If you type 'load' you can load in your last save."
         puts ""
         puts "  _________"
         puts "  |       #{@@parts[:r] if @game.amount_of_mistakes >= 1}"
@@ -141,5 +149,4 @@ class Board
     end
 end
 
-Game.new.play
-
+Game.new.play()
